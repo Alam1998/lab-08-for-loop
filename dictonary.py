22774973515344dc16b97d596fcbebd04901d54f
Md. Alam <shaiankhan77@gmail.com>
2021-04-09 22:57:54 -0400

    lab 8 tasks

---
+++ dictonary.py
@@ -0,0 +1,9 @@
+myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+a = []
+b = []
+for c, d in myData.items():
+  print("key:", c, ', value:', d)
+  a.append(c)
+  b.append(d)
+print("all keys: ", a)
+print("all values", b)
